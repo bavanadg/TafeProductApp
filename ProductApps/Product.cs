922ee16041cc418ceb44a3ff2aaf0f254ace03e4
Dhammika <dhammikaedirisinghe@yahoo.com>
2018-06-08 15:59:22 +0930

    Added the delivery charge 25$

--- ProductApps/Product.cs
+++ ProductApps/Product.cs
@@ -14,6 +14,7 @@ namespace ProductApps
         private decimal delivery;
         private decimal wrapping;
         private decimal gst;
+        private decimal totalCharge;
 
         private decimal Price
         {
@@ -64,6 +65,17 @@ namespace ProductApps
         public void calTotalPayment()
         {
             TotalPayment = Price * Quantity;
+        }
+
+        public decimal TotalCharge
+        {
+            get { return totalCharge; }
+            set { totalCharge = value; }
+        }
+
+        public void calTotalCharge()
+        {
+            TotalCharge = TotalPayment + 25;
         }
     }
 }
